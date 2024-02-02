cd68e85f9f63bdc1d12e77650e33f75844373469
DannyVinueza <danny.vinueza@epn.edu.ec>
2024-02-02 16:30:36 -0500

    Initial commit

---
+++ lib/constants/api_constants.dart
@@ -0,0 +1,3 @@
+const String TMDB_API_BASE_URL = "https://api.themoviedb.org/3";
+const String TMDB_API_KEY = "93910650d04fec5ad8f77a3fb7e650a3";
+const String TMDB_BASE_IMAGE_URL = "https://image.tmdb.org/t/p/";
